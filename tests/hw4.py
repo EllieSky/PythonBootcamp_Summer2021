e5fc064a3b0ac987be618ec1930d24b6bcaf0304
oleksandr <alk24315@gmail.com>
2021-07-22 20:03:31 -0700

    first commit (home work 4)

---
+++ tests/hw4.py
@@ -0,0 +1,38 @@
+number = int(input(f"Please enter your tree height for first task: "))
+
+
+def task_1_make_tree_1(height: int):
+    print("First way (tree trunk DO NOT counted as part of the height):")
+    print(f"Printed numerical tree with height: {number + 1} inch")
+    for x in range(1, height + 1):
+        num = x
+        if x > 9:
+            num = int(str(x)[-1])
+        print(' ' * (height - x) + str(num) * (2 * x - 1))
+    print(' ' * (height - 1) + "|" + "\n")
+
+
+def task_1_make_tree_2(height: int):
+    print("Second way (tree trunk IS counted as part of the height):")
+    print(f"Printed numerical tree with height: {number} inch")
+    for x in range(1, height):
+        num = x
+        if x > 9:
+            num = int(str(x)[-1])
+        print(' ' * (height - x) + str(num) * (2 * x - 1))
+    print(' ' * (height - 1) + "|" + "\n")
+
+
+def task_2_bonus_challenge_func_3(number):
+    print("Bonus challenge task (tree trunk DO NOT counted as part of the height)")
+    print(f"Printed numerical tree with height: {number + 1} inch")
+    for x in range(1, number + 1):
+        for y in range(number - x):
+            print(end=' ')
+        print(pow(((10 ** x - 10) // 9) + 1, 2))
+    print(' ' * (number - 1) + "|" + "\n")
+
+
+task_1_make_tree_1(number)
+task_1_make_tree_2(number)
+task_2_bonus_challenge_func_3(number)
