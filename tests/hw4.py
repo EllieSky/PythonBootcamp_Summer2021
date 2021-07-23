284baab0059a9929b54e725da34a1e5f03ebec84
“rxdai@yahoo.com” <rxdai@yahoo.com>
2021-07-22 20:04:02 -0700

    solution to hw4 : part 1

---
+++ tests/hw4.py
@@ -0,0 +1,12 @@
+import random
+
+def make_number_tree(height_of_tree: int = random.randint(2, 12)):
+    for branch in range(height_of_tree):
+        symbol = branch + 1
+        if symbol >= 10:
+            int(symbol/10)
+        print(' ' * (height_of_tree - branch) + str((branch +1)%10) * (1+(branch *2)))
+        # print(' ' * (height_of_tree - branch) + str(branch + 1) * (1 + (branch * 2)))
+    print(' ' * height_of_tree + '|')
+
+make_number_tree(12)
