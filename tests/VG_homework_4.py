cba7a8551be4ff80b25b244584ec8719921886aa
Vadim Gumarov <vadim.gumarov@gmail.com>
2021-07-23 00:38:59 -0500

    New TC with Webdriver

--- tests/VG_homework_4.py
+++ tests/VG_homework_4.py
@@ -27,13 +27,7 @@ def make_symmetrical_tree(height_of_tree):
     reversed_branch_sequence = []
     for branch in range(height_of_tree):
         decor += 1
-        # if decor > 9:
-        #     new_decor = 0
-        #     branch_sequence = list(range(0, new_decor + 10))
-        #     reversed_branch_sequence = reversed(range(0, new_decor + 10))
-        #     print(' ' * (height_of_tree - branch) +
-        #           "".join([str(x) for x in branch_sequence]) +
-        #           "".join([str(x) for x in reversed_branch_sequence]))
+
         branch_sequence = list(range(0, decor - 1))
         reversed_branch_sequence = reversed(range(0, decor))
         print(' ' * (height_of_tree - branch) +
