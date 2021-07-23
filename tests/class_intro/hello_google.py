74816fc9304c904d1e0afba60c8cc45b166776bc
oleksandr <alk24315@gmail.com>
2021-07-22 22:39:14 -0700

    created webdriver test

--- tests/class_intro/hello_google.py
+++ tests/class_intro/hello_google.py
@@ -4,6 +4,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+<<<<<<< HEAD
 from webdriver_manager.firefox import GeckoDriverManager
 
 
@@ -14,6 +15,15 @@ class GoogleSearchTests(unittest.TestCase):
 
         browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
         browser.get("http://www.google.com")
+=======
+
+
+class MyTestCase(unittest.TestCase):
+    def test_search_hello(self):
+
+        browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        browser.get('http://www.google.com')
+>>>>>>> created webdriver test
         browser.find_element_by_name('q').send_keys('hello', Keys.ENTER)
         sleep(2)
         actual = browser.find_element_by_name('q').get_attribute('value')
@@ -21,5 +31,9 @@ class GoogleSearchTests(unittest.TestCase):
         self.assertEqual('hello', actual)
         pass
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> created webdriver test
 if __name__ == '__main__':
     unittest.main()
