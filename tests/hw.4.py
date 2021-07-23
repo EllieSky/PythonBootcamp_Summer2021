25667a23d29c636a7acf9ba9e3977011e57e77f6
nradaev <nradaev@clearchoice.com>
2021-07-22 21:00:14 -0600

    My test message

---
+++ tests/hw.4.py
@@ -0,0 +1,9 @@
+import random
+
+
+def make_number_tree(height_of_tree: int = random.randint(2, 12):
+    for branch in range(height_of_tree):
+        print(' ' * (height_of_tree - branch) + str(branch + 1)%10 * (1 + (branch * 2)))
+    print(' ' * height_of_tree + '|')
+
+make_number_tree(9)
