62c07cdd6f5c9bd741cc8b14090653ce87d59779
ellie <blackellie@yahoo.com>
2021-07-27 22:29:57 -0700

    class 6: valid_login test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,12 @@
 import unittest
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
-from tests import CHROME_PATH, DOMAIN, ADMIN_USER
+from tests import CHROME_PATH, DOMAIN, ADMIN_USER, DEFAULT_PASSWORD
 
 
 class LoginPageTests(unittest.TestCase):
@@ -15,7 +18,22 @@ class LoginPageTests(unittest.TestCase):
         self.browser.quit()
 
     def test_valid_login(self):
+        original_url = self.browser.current_url
         self.browser.find_element(By.ID, "txtUsername").send_keys(ADMIN_USER)
+        self.browser.find_element(By.ID, "txtPassword").send_keys(DEFAULT_PASSWORD)
+        self.browser.find_element(By.ID, "btnLogin").click()
+
+        wait = WebDriverWait(self.browser, 7)
+        wait.until(expected_conditions.url_contains("/pim/viewEmployeeList"))
+
+        new_url = self.browser.current_url
+        self.assertNotEqual(original_url, new_url)
+
+        welcome_message = self.browser.find_element(By.ID, "welcome").text
+        self.assertEqual("Welcome Admin", welcome_message)
+
+        self.assertEqual("Employee Information",
+                         self.browser.find_element(By.TAG_NAME, "h1").text)
 
 
 if __name__ == '__main__':
