f1ce03732df224b95a0b20535af5d69f482aad22
okov1 <o.kov2143@gmail.com>
2021-08-12 20:25:39 -0700

    class 11

--- features/environment.py
+++ features/environment.py
@@ -1,3 +1,5 @@
+import os
+
 from behave import *
 
 # Possible other setup/teardown functions you can create
@@ -6,9 +8,10 @@ from behave import *
 # before_step / after_scenario
 
 from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 
 from test_steps.base_methods import BaseMethods
-from tests import DOMAIN, CHROME_PATH
+from tests import DOMAIN, CHROME_PATH, PROJ_HOME
 
 
 def before_scenario(context, scenario):
@@ -18,4 +21,18 @@ def before_scenario(context, scenario):
 
 
 def after_scenario(context, scenario):
-    context.browser.quit()
+    browser: WebDriver = context.browser
+
+    if scenario.status.name == 'failed':
+        output_folder_path = os.path.join(PROJ_HOME, 'test_output')
+        os.mkdir(output_folder_path)
+
+        scenario_folder_path = os.path.join(output_folder_path, scenario.name.replace(" ", "_").lower())
+        os.mkdir(scenario_folder_path)
+
+        print(f'Test failed on the page with url {browser.current_url}\n')
+        file = open(os.path.join(scenario_folder_path, 'page_source.html'), 'w')
+        file.write(browser.page_source)
+        file.close()
+        browser.save_screenshot(os.path.join(scenario_folder_path, "test_screenshot.png"))
+    browser.quit()
